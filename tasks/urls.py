3968dd68c2153ae5a451294bd61415b5596beb64
Victor Fedorov <vpfedorov@bk.ru>
2021-10-15 17:54:41 +0300

    tasks' pages initital

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,1 +1,8 @@
 # tasks/urls.py
+from django.urls import path, re_path, include
+from django.conf.urls import url
+from tasks import views
+
+urlpatterns = [
+    url(r"^$", views.index, name="tasks")
+]
