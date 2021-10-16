608d64ac87bbd6c05fd657cf5df14b6b9e6a5dd0
Victor Fedorov <vpfedorov@bk.ru>
2021-10-17 00:13:24 +0300

    data base initial version

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,29 @@
+import datetime
+
 from django.db import models
+from users.models import Employee
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    tasksCount = models.IntegerField(default=0)
+    deadline = models.DateTimeField(default=None)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=30)
+    project = models.ForeignKey(Project, on_delete = models.CASCADE, default=None)
+    description = models.TextField()
+    status = models.IntegerField(blank=True, default=0)
+    deadline = models.DateTimeField(blank=True)
+    # указывает на задачу предка
+    subtask = models.IntegerField(blank=True, default=0)
+    employee = models.ManyToManyField(Employee)
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    employee = models.ForeignKey(Employee, on_delete = models.CASCADE)
+    task = models.ManyToManyField(Task)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class PersonalData(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    position = models.CharField(max_length=30)
+    employee = models.OneToOneField(Employee, on_delete=models.CASCADE, primary_key=True)
