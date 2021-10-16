a237bae02de86845a895939f66ec2dd3400b9bc9
Victor Fedorov <vpfedorov@bk.ru>
2021-10-17 00:12:57 +0300

    db tables in admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from tasks.models import Project, Task, Comment
 # Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from users.models import Employee, PersonalData
 # Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PersonalData)
+class PersonalDataAdmin(admin.ModelAdmin):
+    pass
