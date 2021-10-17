f89aab04320dc16b37bfba44c7189ae976928bae
Victor Fedorov <vpfedorov@bk.ru>
2021-10-17 16:41:24 +0300

    photo for user now avaliable

--- users/models.py
+++ users/models.py
@@ -10,3 +10,4 @@ class PersonalData(models.Model):
     phone = models.CharField(max_length=15)
     position = models.CharField(max_length=30)
     employee = models.OneToOneField(Employee, on_delete=models.CASCADE, primary_key=True)
+    photo = models.FilePathField(path="", blank=True)
