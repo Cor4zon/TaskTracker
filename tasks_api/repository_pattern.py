9eaeb0f2619841b33ce603b361b38fe013a33ecb
Victor Fedorov <vpfedorov25@gmail.com>
2022-01-20 14:06:50 +0300

    Repository Pattern finished

--- tasks_api/repository_pattern.py
+++ tasks_api/repository_pattern.py
@@ -1,28 +1,28 @@
 import abc
+import transaction as transaction
 from tasks.models import Project, Task
-from django.shortcuts import get_object_or_404
 
 
 class AbstractRepository(abc.ABC):
 
     @classmethod
     @abc.abstractmethod
-    def add(cls, item):
+    def create(cls, item):
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
-    def get(cls, pk):
+    def read(cls, pk):
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
-    def get_filtered(cls, filter_dict):
+    def read_filtered(cls, filter_dict):
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
-    def list(cls):
+    def read_all(cls):
         raise NotImplementedError
 
     @classmethod
@@ -32,27 +32,30 @@ class AbstractRepository(abc.ABC):
 
     @classmethod
     @abc.abstractmethod
-    def patch(cls):
+    def update(cls, pk, data):
         raise NotImplementedError
 
 
 class ProjectRepository(AbstractRepository):
     @classmethod
-    # @transaction.atomic
-    def add(cls, project: Project):
+    def create(cls, project_data):
+        project = Project(
+                            title=project_data["title"],
+                            description=project_data["description"],
+                            deadline=project_data["deadline"]
+                         )
         project.save()
 
     @classmethod
-    def get(cls, pk):
-        queryset = Project.objects.filter()
-        return get_object_or_404(queryset, pk=pk)
+    def read(cls, pk):
+        return Project.objects.filter(pk=pk)
 
     @classmethod
-    def get_filtered(cls, filter_dict):
+    def read_filtered(cls, filter_dict):
         pass
 
     @classmethod
-    def list(cls):
+    def read_all(cls):
         return Project.objects.all()
 
     @classmethod
@@ -60,27 +63,32 @@ class ProjectRepository(AbstractRepository):
         Project.objects.filter(pk=pk).delete()
 
     @classmethod
-    def patch(cls):
-        pass
+    def update(cls, pk, data):
+        Project.objects.filter(pk=pk).update(**data)
 
 
 class TaskRepository(AbstractRepository):
     @classmethod
-    # @transaction.atomic
-    def add(cls, task: Task):
+    def create(cls, task_info):
+        task = Task(
+                    title=task_info["title"],
+                    description=task_info["description"],
+                    status=task_info["status"],
+                    deadline=task_info["deadline"],
+                    project_id=task_info["project"]
+                    )
         task.save()
 
     @classmethod
-    def get(cls, task_pk):
-        queryset = Task.objects.filter(pk=task_pk)
-        return get_object_or_404(queryset, pk=task_pk)
+    def read(cls, task_pk):
+        return Task.objects.filter(pk=task_pk)
 
     @classmethod
-    def get_filtered(cls, filter_dict):
+    def read_filtered(cls, filter_dict):
         return Task.objects.filter(**filter_dict)
 
     @classmethod
-    def list(cls):
+    def read_all(cls):
         return Task.objects.all()
 
     @classmethod
@@ -88,31 +96,32 @@ class TaskRepository(AbstractRepository):
         Task.objects.filter(pk=task_pk).delete()
 
     @classmethod
-    def patch(cls):
+    def update(cls, pk, data):
         pass
+
 
 
 class CustomService():
     def __init__(self, repository):
         self.repository = repository
 
-    def add(self, model):
-        self.repository.add(model)
+    def create(self, model):
+        self.repository.create(model)
 
-    def get(self, pk):
-        return self.repository.get(pk)
+    def read(self, pk):
+        return self.repository.read(pk)
 
-    def get_filtered(self, project_pk):
-        return self.repository.get_filtered(project_pk)
+    def read_filtered(self, project_pk):
+        return self.repository.read_filtered(project_pk)
 
-    def list(self):
-        return self.repository.list()
+    def read_all(self):
+        return self.repository.read_all()
 
     def delete(self, pk):
         return self.repository.delete(pk)
 
-    def patch(self):
-        pass
+    def update(self, pk, data):
+        return self.repository.update(pk, data)
 
 
 ProjectService = CustomService(ProjectRepository)
--- tasks_api/views.py
+++ tasks_api/views.py
@@ -13,24 +13,18 @@ class ProjectViewSet(viewsets.ViewSet):
     serializer_class = ProjectSerializer
 
     def list(self, request):
-        # queryset = Project.objects.filter()
-        projects = ProjectService.list()
+        projects = ProjectService.read_all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        project = ProjectService.get(pk)
-        serializer = ProjectSerializer(project)
+        project = ProjectService.read(pk)
+        serializer = ProjectSerializer(project, many=True)
         return Response(serializer.data)
 
     def create(self, request):
-        title = request.POST.get("title")
-        description = request.POST.get("description")
-        deadline = request.POST.get("deadline")
-        new_project = Project(title=title, description=description, tasksCount=0, deadline=deadline)
-
-        ProjectService.add(new_project)
-        # new_project.save()
+        new_project = request.data
+        ProjectService.create(new_project)
         serializer = ProjectSerializer(new_project)
         return Response(serializer.data)
 
@@ -38,31 +32,28 @@ class ProjectViewSet(viewsets.ViewSet):
         ProjectService.delete(pk)
         return JsonResponse({'status': 'OK', 'message': 'You deleted project'}, status=200)
 
+    def partial_update(self, request, pk):
+        ProjectService.update(pk, request.data)
+        return JsonResponse({'status': 'OK', 'message': 'You update project'}, status=200)
+
 
 class TaskViewSet(viewsets.ViewSet):
     serializer_class = TaskSerializer
 
     def list(self, request, project_pk=None, pk=None):
-        tasks = TaskService.get_filtered({"project": project_pk})
+        tasks = TaskService.read_filtered({"project": project_pk})
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, project_pk=None, pk=None):
-        task = TaskService.get(pk)
+        task = TaskService.read(pk)
         serializer = TaskSerializer(task)
         return Response(serializer.data)
 
     def create(self, request, project_pk=None):
-        title = request.data["title"]
-        description = request.data["description"]
-        status = request.data["status"]
-        deadline = request.data["deadline"]
-        new_task = Task(title=title, description=description, deadline=deadline, project_id=project_pk)
-
-        TaskService.add(new_task)
-
-        serializer = TaskSerializer(new_task)
-        return Response(serializer.data)
+        new_task = request.data
+        TaskService.create(new_task)
+        return Response(new_task)
 
     def destroy(self, request, project_pk=None, pk=None):
         TaskService.delete(pk)
