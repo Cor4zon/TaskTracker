ba216511295f57b80c138919bbeafe4539c419b0
Victor Fedorov <vpfedorov25@gmail.com>
2022-01-30 22:09:23 +0300

    CI build 1

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Project, Task, Comment
+from .forms import ProjectForm, TaskForm, CommentForm
+
+
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        # self.user1 = User.objects.create_user(username="admin")
+        Project.objects.create(
+            title="Test Project",
+            description="We are testing this",
+            tasksCount=0,
+            deadline=timezone.now())
+
+    def test_project_is_posted(self):
+        """Posts are created"""
+        project1 = Project.objects.get(title="Test Project")
+        self.assertEqual(project1.description, "We are testing this")
+
+    def test_blank_form_data(self):
+        form = ProjectForm({})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'title': ['This field is required.'],
+            'description': ['This field is required.'],
+            'deadline': ['This field is required.'],
+        })
